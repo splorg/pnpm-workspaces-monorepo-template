f488aa2ba56b1a66e5d318e2adc05b67ca66a5ea
Gabriel Lima <gabriel.lbs002@gmail.com>
2025-01-21 22:39:32 -0300

    feat: add shared eslint config package

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,3 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import prettierPlugin from "eslint-plugin-prettier";
+import config from "./packages/eslint-config/eslint.config.js";
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  { plugins: { prettier: prettierPlugin } },
-  { ignores: ["node_modules", "eslint.config.mjs", "**/dist", "config/*"] },
-];
+export default config;
